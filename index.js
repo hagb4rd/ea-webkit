89bba8752a95d6744e883f289a01cfe850840b5f
hagb4rd <hagb4rd@gmail.com>
2018-09-05 00:43:34 +0200

    $ jquery4

--- index.js
+++ index.js
@@ -1,47 +1,74 @@
-
 //var request = require("request");
 //var rp = require("request-promise");
-var url = require("url");
+var $ = module.exports = require('./lib/jquery');
+
+
+var url = $.url = require("url");
 var path = require("path");
-var querystring = require("querystring");
-var $ = require("jquery");
+var querystring = $.querystring = require("querystring");
 var util = require("util");
 //var xhr = require("./lib/xhr");
 var lib = require("ea-lib");
-var bookmarklet = exports.bookmarklet = require("./lib/bookmarklet");
+Object.assign($,lib);
+var bookmarklet = $.bookmarklet = require("./lib/bookmarklet");
 //var WRGenerator = require("./lib/WRGenerator");
 
+/*
 var window=window||{};
 
-window.webkit=module.exports;
+var module = module||{exports:{}};
+var exports = exports||module.exports;
 
-exports.lib = lib;
+
+window.webkit=module.exports;
+*/
+
 //Object.assign(exports, lib);
 
-exports.EventEmitter = require("events").EventEmitter;
-exports.$ = $;
-exports.querystring = querystring;
-exports.url = url;
-exports.path = path;
-exports.util = util;
-exports.Buffer = Buffer;
+//$.EventEmitter = require("events").EventEmitter;
+$.querystring = querystring;
+$.url = url;
+$.path = path;
+$.util = util;
+$.Buffer = Buffer;
 
-var qs = exports.qs = (function(s) { return this.querySelector(s); }).bind(window && window.document)
-var qsa = exports.qsa = (function(s) { return this.querySelectorAll(s); }).bind(window && window.document)
+var qs = $.qs = (s,elem) => { elem=elem||document; return elem.querySelector(s); };
+var qsa = $.qsa = (s,elem) => { elem=elem||document; return Array.from(elem.querySelectorAll(s)); };
 
-exports.tags = function tags(...taglist) {
+$.tags = function tags(...taglist) {
   var selectors = taglist.map(tag => `a[tags*=${tag}]`);
   var xs = qsa(selectors.join(","));
   return xs;
 }
 
-var isIterable=exports.isIterable=obj=>obj[Symbol.iterator]=="function"||Array.isArray(obj)||typeof obj.length!="undefined"; 
-var each=exports.each = (iterable,f,m={})=>(f&&f.bind&&(f=f.bind(m)),(isIterable(iterable)?[...iterable]:Object.entries(iterable)).forEach(f), m); each([1,2,3],function(x){this.result+=x},{result:0}); 
-var map = exports.map = (iterable, f) => (isIterable(iterable)?[...iterable]:Object.entries(iterable)).map(f);
+var isIterable=$.isIterable=obj=>obj[Symbol.iterator]=="function"||Array.isArray(obj)||typeof obj.length!="undefined"; 
+var each=$.each = (iterable,f,m={})=>(f&&f.bind&&(f=f.bind(m)),(isIterable(iterable)?[...iterable]:Object.entries(iterable)).forEach(f), m); each([1,2,3],function(x){this.result+=x},{result:0}); 
+var map = $.map = (iterable, f) => (isIterable(iterable)?[...iterable]:Object.entries(iterable)).map(f);
 
-//exports.lib = require("./functions");
-//exports.keywords = require("./keywords");
-exports.base64 = {
+//$.lib = require("./functions");
+//$.keywords = require("./keywords");
+$.download = () => async(text, filename="text") => {
+  filename=filename||lib.UUID()
+  var element = document.createElement('a');
+  
+  if(typeof(text)=='object'){
+    if(!filename.endsWith('json'))
+      filename+=".json";
+    text=lib.json.stringify(text);
+  }
+  
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
+}
+$.base64 = {
   fromImage: function fromImage(image, outputFormat) {
 
     var outputFormat = outputFormat || "image/jpeg";
@@ -76,7 +103,3 @@ exports.base64 = {
     })
   }
 };
-
-if(typeof(window)!='undefined') {
-  
-}
